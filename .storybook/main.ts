356f0f9ba78959ba114be6a2a882c91f05496f70
kuro5888 <ben.allan5888@gmail.com>
2024-04-01 18:31:29 +0100

    Added Prettier Set up and created basic hexagon component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,7 +3,7 @@ import type { StorybookConfig } from "@storybook/nextjs";
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@chromatic-com/storybook",
---
+++ src/components/Hexagon/Hexagon.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Hexagon = () => {
+  return <div>Hexagon</div>;
+};
