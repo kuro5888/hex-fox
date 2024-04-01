4e5d92454998f77605fe49688053197997ec120c
kuro5888 <ben.allan5888@gmail.com>
2024-04-01 22:39:09 +0100

    V1 Hexagon

--- src/components/Hexagon/Hexagon.tsx
+++ src/components/Hexagon/Hexagon.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
 export const Hexagon = () => {
-  return <div>Hexagon</div>;
+  return (
+    <p>
+      <svg height="110" width="300">
+        <polygon
+          points="50 3, 100 28, 100 75, 50 100, 3 75, 3, 25"
+          stroke="black"
+          fill-opacity="0.0"
+          stroke-width="5"
+        />
+      </svg>
+      <div>Hexagon</div>
+    </p>
+  );
 };
